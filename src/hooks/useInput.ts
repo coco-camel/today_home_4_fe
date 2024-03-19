219e333016898e54581d3f0c07f1577a95bba989
Juno <anint3647@gmail.com>
2024-03-19 23:50:37 +0900

    project 초기 세팅

---
+++ src/hooks/useInput.ts
@@ -0,0 +1,10 @@
+// import { useState } from 'react';
+
+// export const useInput = () => {
+//   const [value, setValue] = useState('');
+//   const onChangeHandler = (e) => {
+//     setValue(e.target.value);
+//   };
+
+//   return [value, onChangeHandler];
+// };
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { RouterProvider } from 'react-router-dom';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
+import { router } from './routes/Router';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById('root') as HTMLElement,
 );
+const queryClient = new QueryClient();
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <RouterProvider router={router} />
+  </QueryClientProvider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
