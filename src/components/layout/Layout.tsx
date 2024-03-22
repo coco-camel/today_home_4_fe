aa527d626872d3d1c79d8b805c3e86c894c738a1
asdfg030906 <115532928+asdfg030906@users.noreply.github.com>
2024-03-23 00:43:40 +0900

    코드 수정

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
-import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
   return (
