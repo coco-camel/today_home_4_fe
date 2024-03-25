f37737c3d1b847289aa18bcee15c977e28a33583
함석원 <s1@hamseog-won-ui-Macmini.local>
2024-03-25 19:09:16 +0900

    💄main 북마크작업 완료

--- src/components/feature/main/Products.tsx
+++ src/components/feature/main/Products.tsx
@@ -6,6 +6,7 @@ import productAPI from '../../../apis/product';
 import {
   useInfiniteQuery,
   useMutation,
+  useQueryClient,
 } from '@tanstack/react-query';
 import { Link } from 'react-router-dom';
 import { Product } from '../../../interfaces/product/product.interface';
@@ -63,7 +64,7 @@ const Products = () => {
   ) => {
     setIsFreeDelivery(!isFreeDelivery);
   };
-
+  const queryClient = useQueryClient();
   // 북마크 등록
   const addBookMark = async (
     productId: number,
@@ -72,10 +73,11 @@ const Products = () => {
       await bookMarkAPI.addBookMark(productId);
     return data.data;
   };
-
   const { mutate: createMutate } = useMutation({
     mutationFn: addBookMark,
-    onSuccess: () => {},
+    onSuccess: () => {
+      queryClient.invalidateQueries('projects');
+    },
   });
   //북마크 삭제
   const delBookMark = async (
@@ -87,21 +89,21 @@ const Products = () => {
   };
   const { mutate: deleteMutate } = useMutation({
     mutationFn: delBookMark,
-    onSuccess: () => {},
+    onSuccess: () => {
+      queryClient.invalidateQueries('projects');
+    },
   });
 
-  const handleBookmarkClick = (
+  const handleAddBookmarkClick = (
     productId: number,
-    isBookmarked: boolean,
   ) => {
     createMutate(productId);
-    if (isBookmarked === false) {
-      createMutate(productId);
-    } else {
-      deleteMutate(productId);
-    }
   };
-
+  const handleDelBookmarkClick = (
+    productId: number,
+  ) => {
+    deleteMutate(productId);
+  };
   if (!products) {
     return <div>Loading...</div>;
   }
@@ -109,12 +111,12 @@ const Products = () => {
   return (
     <ProductSection>
       <h1>인기 상품</h1>
-      <FilterButton
+      {/* <FilterButton
         onFreeDeliveryClick={
           handleFreeDeliveryClick
         }
         isTrue={isFreeDelivery}
-      />
+      /> */}
       <ProductWrap>
         <InfiniteScroll
           style={{ overflow: 'hidden' }}
@@ -136,15 +138,137 @@ const Products = () => {
                     src={product.imageUrl}
                     alt=""
                   />
+
                   <ScrapBtn>
-                    <button
-                      onClick={() =>
-                        handleBookmarkClick(
-                          product.productId,
-                          product.isBookmarked,
-                        )
-                      }
-                    ></button>
+                    {!product.isBookmarked ? (
+                      <button
+                        onClick={() =>
+                          handleAddBookmarkClick(
+                            product.productId,
+                          )
+                        }
+                      >
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="24"
+                          height="24"
+                          viewBox="0 0 24 24"
+                          className="inactive-icon"
+                        >
+                          <defs>
+                            <path
+                              id="scrap-icon-2-b"
+                              d="M12.472 6.93l7.056-3.811A1 1 0 0 1 21 4.002v15.496c0 .83-.672 1.502-1.5 1.502h-15c-.828 0-1.5-.673-1.5-1.502V4.002a1 1 0 0 1 1.472-.883l7.056 3.811a.999.999 0 0 0 .944 0z"
+                            ></path>
+                            <filter
+                              id="scrap-icon-2-a"
+                              width="150%"
+                              height="150%"
+                              x="-25%"
+                              y="-25%"
+                              filterUnits="objectBoundingBox"
+                            >
+                              <feOffset
+                                in="SourceAlpha"
+                                result="shadowOffsetOuter1"
+                              ></feOffset>
+                              <feGaussianBlur
+                                in="shadowOffsetOuter1"
+                                result="shadowBlurOuter1"
+                                stdDeviation="1.5"
+                              ></feGaussianBlur>
+                              <feComposite
+                                in="shadowBlurOuter1"
+                                in2="SourceAlpha"
+                                operator="out"
+                                result="shadowBlurOuter1"
+                              ></feComposite>
+                              <feColorMatrix
+                                in="shadowBlurOuter1"
+                                values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.26 0"
+                              ></feColorMatrix>
+                            </filter>
+                            <filter
+                              id="scrap-icon-2-c"
+                              width="150%"
+                              height="150%"
+                              x="-25%"
+                              y="-25%"
+                              filterUnits="objectBoundingBox"
+                            >
+                              <feGaussianBlur
+                                in="SourceAlpha"
+                                result="shadowBlurInner1"
+                                stdDeviation="1.5"
+                              ></feGaussianBlur>
+                              <feOffset
+                                in="shadowBlurInner1"
+                                result="shadowOffsetInner1"
+                              ></feOffset>
+                              <feComposite
+                                in="shadowOffsetInner1"
+                                in2="SourceAlpha"
+                                k2="-1"
+                                k3="1"
+                                operator="arithmetic"
+                                result="shadowInnerInner1"
+                              ></feComposite>
+                              <feColorMatrix
+                                in="shadowInnerInner1"
+                                values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.2 0"
+                              ></feColorMatrix>
+                            </filter>
+                          </defs>
+                          <g
+                            fill="none"
+                            fillRule="nonzero"
+                            transform="matrix(1 0 0 -1 0 24)"
+                          >
+                            <use
+                              fill="#000"
+                              filter="url(#scrap-icon-2-a)"
+                              href="#scrap-icon-2-b"
+                            ></use>
+                            <use
+                              fill="#FFF"
+                              fillOpacity=".4"
+                              href="#scrap-icon-2-b"
+                            ></use>
+                            <use
+                              fill="#000"
+                              filter="url(#scrap-icon-2-c)"
+                              href="#scrap-icon-2-b"
+                            ></use>
+                            <path
+                              stroke="#FFF"
+                              d="M12.71 7.37h-.002a1.5 1.5 0 0 1-1.417 0L4.236 3.56a.499.499 0 0 0-.736.442v15.496c0 .553.448 1.002 1 1.002h15c.552 0 1-.449 1-1.002V4.002a.499.499 0 0 0-.734-.443l-7.057 3.81zm-.475-.88h-.001z"
+                            ></path>
+                          </g>
+                        </svg>
+                      </button>
+                    ) : (
+                      <button
+                        onClick={() =>
+                          handleDelBookmarkClick(
+                            product.productId,
+                          )
+                        }
+                      >
+                        <svg
+                          className="active-icon"
+                          width="24"
+                          height="24"
+                          viewBox="0 0 24 24"
+                          preserveAspectRatio="xMidYMid meet"
+                        >
+                          <path
+                            fill="#35C5F0"
+                            fillRule="nonzero"
+                            d="M12.472 17.07a.999.999 0 0 0-.944 0l-7.056 3.811A.999.999 0 0 1 3 19.998V4.502C3 3.672 3.672 3 4.5 3h15c.828 0 1.5.673 1.5 1.502v15.496a1 1 0 0 1-1.472.883l-7.056-3.811z"
+                          ></path>
+                        </svg>
+                      </button>
+                    )}
                   </ScrapBtn>
                 </ImgWrap>
                 <ItemInfo>
@@ -252,7 +376,6 @@ const ScrapBtn = styled.div`
   button {
     width: 24px;
     height: 24px;
-    background: red;
   }
   display: block;
   position: absolute;
